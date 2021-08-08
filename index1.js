3edccb444811335521ba50ab15cfca5033cd606b
Muthukumar <tmuthukumrnpm@gmail.com>
2021-08-08 19:47:48 +0530

    Initial Commit

---
+++ index1.js
@@ -0,0 +1,22 @@
+const slider = document.querySelectorAll('.section');
+const sliderdots = document.querySelectorAll('.dot');
+let slideindex = 0;
+showslide();
+
+function showslide(){
+    console.log(slideindex);
+    for(var i=0;i<slider.length;i++){
+        slider[i].style.display = 'none';
+    }
+    for(var j=0;j<sliderdots.length;j++){
+        sliderdots[j].style.backgroundColor = 'white';
+    }
+    sliderdots[slideindex].style.backgroundColor = 'blue';
+    slider[slideindex].style.display = 'flex';
+    slideindex++;
+    if(slideindex == slider.length){
+        console.log(slideindex);
+        slideindex = 0;
+    }  
+    setTimeout(showslide, 2000);  
+}
