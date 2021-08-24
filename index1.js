78bb3701367aede4d1ac05cfe43327e0333ebffd
Muthukumar <tmuthukumrnpm@gmail.com>
2021-08-24 09:10:47 +0530

    Updated  Final Version

--- index1.js
+++ index1.js
@@ -29,7 +29,7 @@ document.addEventListener('DOMContentLoaded',()=>{
         console.log(top);
         if(top>100){
             navbar.style.backgroundColor = 'white';
-            navbarmillnamelist.innerHTML = 'Rajalakshmi Rice Mill';
+            // navbarmillnamelist.innerHTML = 'Rajalakshmi Rice Mill';
             
         }
         else{
@@ -65,5 +65,44 @@ btnsubmit.addEventListener('click',()=>{
     alert('Development is in progress');
 })
 
+function mapContent(){
+    var api_key = 'YOUR_API_KEY';
+    var latAndLong = {lat: 11.0214697001936, lng:  77.53150516931241}; 
+    var zoomLevel = 14;
+    var yourAddress = 'Rajalakshmi Modern Rice Mill, 1147/3A-1, Ramapattinam Road, Sivanmalai POST, Kangeyam, Tiruppur, Tamil Nadu, 638701';
+
+    var map = tt.map({
+        container: 'map',
+        key: 'DXtV3JoDYm7aFQbbCXAlAqGnwrn8nlND',
+        center: latAndLong,
+        zoom: zoomLevel
+    });
+
+    var marker = new tt.Marker().setLngLat(latAndLong).addTo(map);
+     
+     // FOR CUSTOM MARKER
+    //var customMarker = document.createElement('div');
+    //customMarker.id = 'marker';
+    //var marker = new tt.Marker({element: customMarker}).setLngLat(latAndLong).addTo(map);
+
+    var popupOffsets = {
+      top: [0, 0],
+      bottom: [0, -70],
+      'bottom-right': [0, -70],
+      'bottom-left': [0, -70],
+      left: [25, -35],
+      right: [-25, -35]
+    }
+
+    var popup = new tt.Popup({offset: popupOffsets}).setHTML(yourAddress);
+    marker.setPopup(popup).togglePopup();
+}
+
+const getdir = document.querySelector('#getdirection');
+
+getdir.addEventListener('click',()=>{
+    //location.replace("maps.html");
+    window.open('maps.html', '_blank').focus();
+})
 
 
